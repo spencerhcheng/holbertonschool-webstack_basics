742a53361bff4d00e82f249efd89a1bc0efbfadb
Spencer Cheng <spencerhcheng@gmail.com>
2018-05-18 22:33:57 +0000

    7-max_integer: initial commit

---
+++ 0x01-python_basics/7-max_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def max_integer(my_list=[]):
+    """
+    Finds the biggest integer
+    of a list. If list is empty
+    returns None
+    """
+    if len(my_list) == 0:
+        return None
+    max_num = my_list[0]
+    for num in my_list:
+        if num >= max_num:
+            max_num = num
+    return max_num
