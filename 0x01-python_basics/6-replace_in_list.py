8519f880b623b2bb209ed5f1e4e242fa92c7c0fe
Spencer Cheng <spencerhcheng@gmail.com>
2018-05-18 22:31:09 +0000

    6-replace_in_list: initial commit

---
+++ 0x01-python_basics/6-replace_in_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def replace_in_list(my_list, idx, element):
+    """
+    Replaces element of a list
+    at a specific position idx
+    """
+    lst_len = len(my_list)
+    if idx >= lst_len:
+        return my_list
+    else:
+        my_list[idx] = element
+    return my_list
