d59b3b28979f6194027a67f5d922a64cc3c651c4
Spencer Cheng <spencerhcheng@gmail.com>
2018-05-25 23:02:00 +0000

    103-new_in_list: check if idx is out of range

--- 0x01-python_basics/103-new_in_list.py
+++ 0x01-python_basics/103-new_in_list.py
@@ -6,6 +6,10 @@ def new_in_list(my_list, idx, element):
     Replaces element in a list at a specific
     position without modifyiing original list
     """
+
     new_list = my_list[:]
+    __len = len(my_list)
+    if idx > __len - 1 or __len < 0:
+        return new_list
     new_list[idx] = element
     return new_list
