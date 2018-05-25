92210b5058d742c5c5fbb49b4dceb7cc441c0af8
Spencer Cheng <spencerhcheng@gmail.com>
2018-05-25 17:30:48 +0000

    2-islower: move comment below def

--- 0x01-python_basics/2-islower.py
+++ 0x01-python_basics/2-islower.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 
-"""
-Checks if a character is
-lowercase
-"""
 def islower(c):
+    """
+    Checks if a character is
+    lowercase
+    """
     if ord(c) > 96 and ord(c) < 123:
         return True
     else:
