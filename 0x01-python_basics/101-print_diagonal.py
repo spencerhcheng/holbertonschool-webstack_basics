39a39aab3396952268860a164efd0a5903478da6
Spencer Cheng <spencerhcheng@gmail.com>
2018-05-25 20:42:53 +0000

    101-print_diagonal: move print of extra line

--- 0x01-python_basics/101-print_diagonal.py
+++ 0x01-python_basics/101-print_diagonal.py
@@ -6,10 +6,11 @@ def print_diagonal(n):
     Draws a diagonal line on the terminal
     """
     if n <= 0:
-        print('\n')
+        print('')
     else:
         count = 0
         for idx in range(n):
             print('{}'.format(count * ' '), end="")
             count += 1
             print('\\')
+        print('')
