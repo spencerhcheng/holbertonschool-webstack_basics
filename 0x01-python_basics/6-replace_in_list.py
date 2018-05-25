769def84e780f03e4f81f3d3a116a8ed59545f4c
Spencer Cheng <spencerhcheng@gmail.com>
2018-05-25 17:38:35 +0000

    6-replace_in_list: check if idx is less than 0

--- 0x01-python_basics/6-replace_in_list.py
+++ 0x01-python_basics/6-replace_in_list.py
@@ -7,7 +7,7 @@ def replace_in_list(my_list, idx, element):
     at a specific position idx
     """
     lst_len = len(my_list)
-    if idx >= lst_len:
+    if idx >= lst_len or idx < 0:
         return my_list
     else:
         my_list[idx] = element
