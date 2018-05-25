a34cdeb7d8f4c9c784e3d664a3df77548683c94d
Spencer Cheng <spencerhcheng@gmail.com>
2018-05-25 17:25:41 +0000

    0-print_alphabt: move outside of function

--- 0x01-python_basics/0-print_alphabt.py
+++ 0x01-python_basics/0-print_alphabt.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 
 
-def print_alphabt():
-    """
-    Prints alphabets (except for q and e)
-    in lowercase, not followed by a new line
-    """
-    start = 97
+"""
+Prints alphabets (except for q and e)
+in lowercase, not followed by a new line
+"""
+start = 97
 
-    while (start <= 122):
-        if start == 101 or start == 113:
-            start += 1
-            continue
-        print('{}'.format(chr(start)), end="")
+while (start <= 122):
+    if start == 101 or start == 113:
         start += 1
+        continue
+    print('{}'.format(chr(start)), end="")
+    start += 1
