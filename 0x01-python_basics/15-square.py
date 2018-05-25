e54c95c18c28a367160e9de2a249dfee3fd23409
Spencer Cheng <spencerhcheng@gmail.com>
2018-05-25 17:53:36 +0000

    15-suare: test for size in init

--- 0x01-python_basics/15-square.py
+++ 0x01-python_basics/15-square.py
@@ -6,7 +6,12 @@ class Square(object):
         """
         Instantiation with required size
         """
-        self.__size = size
+        if not type(size) == int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
 
     @property
     def size(self):
