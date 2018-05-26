391c4eb37fbc75bfb8c48a699a77aaa2a6e0e758
Spencer Cheng <spencerhcheng@gmail.com>
2018-05-26 00:21:56 +0000

    103-new_in_list: revert back

--- 0x01-python_basics/103-new_in_list.py
+++ 0x01-python_basics/103-new_in_list.py
@@ -7,8 +7,8 @@ def new_in_list(my_list, idx, element):
     position without modifyiing original list
     """
     new_list = my_list[:]
+    __len = len(my_list)
     if idx > __len - 1 or __len < 0:
         return new_list
-    __len = len(my_list)
     new_list[idx] = element
     return new_list
